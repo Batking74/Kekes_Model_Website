9ada15ba67854a485c9aedeaad8a030e69ca518f
Nazir <Batking74@gmail.com>
2022-05-01 02:35:43 -0400

    Finished the markup and css for the login page. Have to finnish form validation and much more.

--- JAVASCRIPT/signIn.js
+++ JAVASCRIPT/signIn.js
@@ -1,1 +1,22 @@
-document.querySelectorAll('#validationForm input').value
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const error = document.getElementById('error');
+const checkbox = document.getElementById('checkbox');
+
+// This is what I'm saying to the computer "Form listen out for a submit, and when you hear a submit prevent the form from submitting/don't submit it."
+form.addEventListener('submit', (stop) => {
+    var messages = []
+    if(email.value === ' ' || email.value > 20 || email.value < 7 || email.value == null) {
+        messages.push('Hello Finnish up!');
+    }
+
+    if(password.value.length >= 16) {
+        messages.push('Password must be shorter!');
+    }
+
+    if(messages.length > 0) {
+        stop.preventDefault();
+        error.innerHTML = messages.join(', ');
+    }
+});
